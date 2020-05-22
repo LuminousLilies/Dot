b2b56a50773244eec1e8147900d21bbe4d498d38
LuminousLilies <LuminousLily@zoho.com>
2020-05-21 20:28:44 -0700

    Self Generated

--- .profile.sh
+++ .profile.sh
@@ -3,4 +3,5 @@ DIR="$1"
 
 # Pass DIR to the aliases to reference itself in aliases
 [[ ! -f "$DIR/.aliases.sh" ]] || . "$DIR/.aliases.sh" $DIR
+[[ ! -f "$DIR/.computer_aliases.sh" ]] || . "$DIR/.computer_aliases.sh" $DIR
 [[ ! -f "$DIR/.exports.sh" ]] || . "$DIR/.exports.sh"

18d3e26a58814e5d1cfef66b049760c7191c0f01
LuminousLilies <luminouslily@zoho.com>
2021-01-11 20:25:14 -0800

    final

--- remove-bak.sh
+++ remove-bak.sh
@@ -1,12 +1,1 @@
-cd ~
-
-# Remove Backup Files
-rm .aliases.sh.bak
-rm .bashrc.bak
-rm .computer_aliases.sh.bak
-rm .exports.sh.bak
-rm .profile.sh.bak
-rm .temp_aliases.sh.bak
-rm .zshrc.bak
-
-cd -
+rm -rf backup
